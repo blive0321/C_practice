6dbc8846eb09433eaf072312a7f38124b0ccece7
blive0321 <BrandonHao0321@gmail.com>
2021-04-12 11:55:07 +0800

    bubble_sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -22,12 +22,13 @@ void show(int a[])
         printf("%d ",a[i]);
     printf("\n");
 }
+//巢狀for + if比相鄰
 void bubble(int a[])
 {
     int i, j, temp;
     for ( i = 1; i < SIZE; i++)  //i控制搜尋次數
     {
-        for ( j = 0; j < (SIZE-i); j++)  //j用來控制相鄰2個數做比較
+        for ( j = 0; j < (SIZE-i); j++)  //j控制相鄰2數比較, 每檢查過一排, 次數在減少1次
             if (a[j]>a[j+1])  //如果第1個元素 比 第2個元素大
             {
                 temp=a[j];
